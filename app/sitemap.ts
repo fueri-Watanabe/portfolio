ce4c50815818b48a467ab6632934cb6522ce63e4
fueri <watanabe@fueri.jp>
2024-01-11 20:01:48 +0900

    fix - sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -1,13 +1,10 @@
 import { MetadataRoute } from "next";
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  const baseUrl = process.env.PUBLIC_URL || "";
-  const lastModified = new Date();
-
   const staticPaths = [
     {
-      url: baseUrl,
-      lastModified,
+      url: "https://fueri.jp/",
+      lastModified: new Date(),
     },
   ];
   return staticPaths;
