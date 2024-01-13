ccb82a1285b9dd8697b42dea8f7712baf802b9fa
fueri <watanabe@fueri.jp>
2024-01-14 03:09:38 +0900

    update: service

--- app/_const/iconSauce.tsx
+++ app/_const/iconSauce.tsx
@@ -5,30 +5,37 @@ export const IconSauce = {
       TypeScript: {
         iconName: "TypeScript",
         src: "/devIcons/typescript-icon.svg",
+        main: true,
       },
       JavaScript: {
         iconName: "JavaScript",
         src: "/devIcons/javascript.svg",
+        main: true,
       },
       HTML5: {
         iconName: "HTML5",
         src: "/devIcons/html-5.svg",
+        main: true,
       },
       CSS3: {
         iconName: "CSS3",
         src: "/devIcons/css-3.svg",
+        main: true,
       },
       GoogleAppsScript: {
         iconName: "GAS",
         src: "/devIcons/gas-icon.svg",
+        main: true,
       },
       Python: {
         iconName: "Python",
         src: "/devIcons/python.svg",
+        main: false,
       },
       PHP: {
         iconName: "PHP",
         src: "/devIcons/php.svg",
+        main: false,
       },
     },
   },
@@ -38,22 +45,27 @@ export const IconSauce = {
       Nextjs: {
         iconName: "Next.js",
         src: "/devIcons/nextjs-icon.svg",
+        main: true,
       },
       React: {
         iconName: "React",
         src: "/devIcons/react.svg",
+        main: true,
       },
       Jotai: {
         iconName: "Jotai",
         src: "/devIcons/jotai-icon.svg",
+        main: true,
       },
       TailwindCSS: {
         iconName: "Tailwind-CSS",
         src: "/devIcons/tailwindcss-icon.svg",
+        main: true,
       },
       Bootstrap: {
         iconName: "Bootstrap",
         src: "/devIcons/bootstrap.svg",
+        main: true,
       },
     },
   },
@@ -63,26 +75,32 @@ export const IconSauce = {
       Prisma: {
         iconName: "Prisma",
         src: "/devIcons/prisma.svg",
+        main: true,
       },
       Vercel: {
         iconName: "Vercel",
         src: "/devIcons/vercel-icon.svg",
+        main: true,
       },
       CloudRun: {
         iconName: "Cloud Run",
         src: "/devIcons/cloud_run.svg",
+        main: true,
       },
       Nodejs: {
         iconName: "Node.js",
         src: "/devIcons/nodejs-icon.svg",
+        main: false,
       },
       MySQL: {
         iconName: "MySQL",
         src: "/devIcons/mysql-icon.svg",
+        main: false,
       },
       Firebase: {
         iconName: "Firebase",
         src: "/devIcons/firebase.svg",
+        main: false,
       },
     },
   },
--- app/components/main/service.tsx
+++ app/components/main/service.tsx
@@ -3,7 +3,7 @@
 import Image from "next/image";
 import { IconSauce } from "../../_const/iconSauce";
 import { CodeBracketSquareIcon } from "@heroicons/react/24/outline";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 
 const serviceContents = [
   "Webサイト",
@@ -14,6 +14,19 @@ const serviceContents = [
 ];
 
 const Service = () => {
+  // const textLightOff = "flex items-center text-gray-500";
+  const textLightOn = "flex items-center";
+  // const [contentClass, setContentClass] = useState(0);
+
+  // useEffect(() => {
+  //   const intervalId = setInterval(() => {
+  //     setContentClass((prev) => (prev == 5 ? 0 : prev + 1));
+  //   }, 1000);
+  //   return () => {
+  //     clearInterval(intervalId);
+  //   };
+  // }, [contentClass]);
+
   return (
     <div className="container flex flex-col lg:flex-row justify-between items-center gap-10 lg:gap-0 lg:h-auto">
       <div className="flex flex-col justify-between gap-8 lg:w-1/2 h-full">
@@ -21,9 +34,13 @@ const Service = () => {
         <ul className="flex flex-col gap-3 text-xl">
           {serviceContents.map((value, index) => {
             return (
-              <li key={index} className="flex">
-                <CodeBracketSquareIcon className="h-6 w-6 text-gray-500 hover:text-gray-900 hover:dark:text-gray-300" />
-                {value}
+              <li
+                key={index}
+                className={textLightOn}
+                // className={contentClass > index ? textLightOn : textLightOff}
+              >
+                <CodeBracketSquareIcon className="h-6 w-6" />
+                <p>{value}</p>
               </li>
             );
           })}
@@ -68,30 +85,38 @@ const Service = () => {
   );
 };
 
-const IconBox = ({ icon }: { icon: { iconName: string; src: string } }) => {
+const IconBox = ({
+  icon,
+}: {
+  icon: { iconName: string; src: string; main: boolean };
+}) => {
   const [hover, setHover] = useState(false);
   return (
-    <div className="relative flex flex-col basis-1/5 items-center p-1 lg:p-2">
-      {hover && (
-        <div className="absolute top-0 -translate-y-6 bg-slate-600 rounded">
-          <p className="text-white text-xs p-1">{icon.iconName}</p>
+    <>
+      {icon.main && (
+        <div className="relative flex flex-col basis-1/5 items-center p-1 lg:p-2">
+          {hover && (
+            <div className="absolute top-0 -translate-y-6 bg-slate-600 rounded">
+              <p className="text-white text-xs p-1">{icon.iconName}</p>
+            </div>
+          )}
+          <div
+            className="rounded-full bg-slate-100 dark:bg-slate-700 hover:bg-slate-200 hover:dark:bg-slate-600 duration-300 p-3"
+            onMouseOver={() => setHover(true)}
+            onMouseLeave={() => setHover(false)}
+            onClick={() => setHover(true)}
+          >
+            <Image
+              src={icon.src}
+              alt={icon.iconName}
+              width={45}
+              height={45}
+              className="lg:h-10 lg:w-10 h-7 w-7 hover:scale-125 duration-200"
+            />
+          </div>
         </div>
       )}
-      <div
-        className="rounded-full bg-slate-100 dark:bg-slate-700 hover:bg-slate-200 hover:dark:bg-slate-600 duration-300 p-3"
-        onMouseOver={() => setHover(true)}
-        onMouseLeave={() => setHover(false)}
-        onClick={() => setHover(true)}
-      >
-        <Image
-          src={icon.src}
-          alt={icon.iconName}
-          width={45}
-          height={45}
-          className="lg:h-10 lg:w-10 h-7 w-7 hover:scale-125 duration-200"
-        />
-      </div>
-    </div>
+    </>
   );
 };
 
