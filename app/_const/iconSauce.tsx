708be973c10a31e3a80f0b115dc32471994ff029
fueri <watanabe@fueri.jp>
2024-02-10 08:21:29 +0900

    update

--- app/_const/iconSauce.tsx
+++ app/_const/iconSauce.tsx
@@ -16,24 +16,10 @@ import {
   SiMysql,
   SiFirebase,
   SiGithub,
-  SiJavascriptHex,
-  SiTypescriptHex,
-  SiHtml5Hex,
-  SiCss3Hex,
-  SiGoogleappsscriptHex,
-  SiPythonHex,
-  SiPhpHex,
-  SiNextdotjsHex,
-  SiReactHex,
-  SiTailwindcssHex,
-  SiBootstrapHex,
-  SiPrismaHex,
-  SiVercelHex,
-  SiNodedotjsHex,
-  SiMysqlHex,
-  SiFirebaseHex,
-  SiGithubHex,
+  SiPlanetscale,
 } from "@icons-pack/react-simple-icons";
+
+const svgClassName = "w-16 h-16";
 
 export const IconSauce = {
   PGLanguage: {
@@ -42,36 +28,43 @@ export const IconSauce = {
       TypeScript: {
         iconName: "TypeScript",
         src: "/devIcons/typescript-icon.svg",
+        svg: <SiTypescript className={svgClassName} />,
         main: true,
       },
       JavaScript: {
         iconName: "JavaScript",
         src: "/devIcons/javascript.svg",
+        svg: <SiJavascript className={svgClassName} />,
         main: true,
       },
       HTML5: {
         iconName: "HTML5",
         src: "/devIcons/html-5.svg",
+        svg: <SiHtml5 className={svgClassName} />,
         main: true,
       },
       CSS3: {
         iconName: "CSS3",
         src: "/devIcons/css-3.svg",
+        svg: <SiCss3 className={svgClassName} />,
         main: true,
       },
       GoogleAppsScript: {
         iconName: "GAS",
         src: "/devIcons/gas-icon.svg",
+        svg: <SiGoogleappsscript className={svgClassName} />,
         main: true,
       },
       Python: {
         iconName: "Python",
         src: "/devIcons/python.svg",
+        svg: <SiPython className={svgClassName} />,
         main: false,
       },
       PHP: {
         iconName: "PHP",
         src: "/devIcons/php.svg",
+        svg: <SiPhp className={svgClassName} />,
         main: false,
       },
     },
@@ -82,26 +75,31 @@ export const IconSauce = {
       Nextjs: {
         iconName: "Next.js",
         src: "/devIcons/nextjs-icon.svg",
+        svg: <SiNextdotjs className={svgClassName} />,
         main: true,
       },
       React: {
         iconName: "React",
         src: "/devIcons/react.svg",
+        svg: <SiReact className={svgClassName} />,
         main: true,
       },
       Jotai: {
         iconName: "Jotai",
         src: "/devIcons/jotai-icon.svg",
+        svg: null,
         main: true,
       },
       TailwindCSS: {
         iconName: "Tailwind-CSS",
         src: "/devIcons/tailwindcss-icon.svg",
+        svg: <SiTailwindcss className={svgClassName} />,
         main: true,
       },
       Bootstrap: {
         iconName: "Bootstrap",
         src: "/devIcons/bootstrap.svg",
+        svg: <SiBootstrap className={svgClassName} />,
         main: true,
       },
     },
@@ -112,36 +110,49 @@ export const IconSauce = {
       Prisma: {
         iconName: "Prisma",
         src: "/devIcons/prisma.svg",
+        svg: <SiPrisma className={svgClassName} />,
+        main: true,
+      },
+      PlanetScale: {
+        iconName: "PlanetScale",
+        src: "/devIcons/planetscale-logo.svg",
+        svg: <SiPlanetscale className={svgClassName} />,
         main: true,
       },
       Vercel: {
         iconName: "Vercel",
         src: "/devIcons/vercel-icon.svg",
+        svg: <SiVercel className={svgClassName} />,
         main: true,
       },
       CloudRun: {
         iconName: "Cloud Run",
         src: "/devIcons/cloud_run.svg",
+        svg: null,
         main: true,
       },
       Nodejs: {
         iconName: "Node.js",
         src: "/devIcons/nodejs-icon.svg",
+        svg: <SiNodedotjs className={svgClassName} />,
         main: false,
       },
       MySQL: {
         iconName: "MySQL",
         src: "/devIcons/mysql-icon.svg",
+        svg: <SiMysql className={svgClassName} />,
         main: false,
       },
       Firebase: {
         iconName: "Firebase",
         src: "/devIcons/firebase.svg",
+        svg: <SiFirebase className={svgClassName} />,
         main: false,
       },
       Github: {
         iconName: "Github",
         src: "/devIcons/github-mark.svg",
+        svg: <SiGithub className={svgClassName} />,
         main: false,
       },
     },
--- app/components/main/service.tsx
+++ app/components/main/service.tsx
@@ -80,43 +80,36 @@ const Service = () => {
         </div>
         <div className="p-2">
           <div className="flex flex-col gap-3">
-            <div>
-              <p className="text-2xl font-semibold">
-                {IconSauce.PGLanguage.title}
-              </p>
-              <div className="flex items-center flex-wrap">
-                {Object.values(IconSauce.PGLanguage.icon).map(
-                  (value, index) => {
-                    return <IconBox key={index} icon={value} />;
-                  }
-                )}
-              </div>
-            </div>
-            <div>
-              <p className="text-2xl font-semibold">
-                {IconSauce.Frontend.title}
-              </p>
-              <div className="flex items-center flex-wrap">
-                {Object.values(IconSauce.Frontend.icon).map((value, index) => {
-                  return <IconBox key={index} icon={value} />;
-                })}
-              </div>
-            </div>
-            <div>
-              <p className="text-2xl font-semibold">
-                {IconSauce.BackendDBCloud.title}
-              </p>
-              <div className="flex items-center flex-wrap">
-                {Object.values(IconSauce.BackendDBCloud.icon).map(
-                  (value, index) => {
-                    return <IconBox key={index} icon={value} />;
-                  }
-                )}
-              </div>
-            </div>
+            {Object.values(IconSauce).map((iconSauceValue, index) => {
+              return (
+                <div key={index}>
+                  <p className="text-2xl font-semibold">
+                    {iconSauceValue.title}
+                  </p>
+                  <div className="flex items-center flex-wrap">
+                    {Object.values(iconSauceValue.icon).map((value) => {
+                      return <IconBox key={value.iconName} icon={value} />;
+                    })}
+                  </div>
+                </div>
+              );
+            })}
           </div>
         </div>
       </div>
+      {/* <div className="flex justify-center items-center flex-wrap">
+        {Object.values(IconSauce).map((iconSauceValue) => {
+          return Object.values(iconSauceValue.icon).map((iconValues) => {
+            return iconValues.svg ? (
+              <li key={iconValues.iconName} className="mx-8">
+                {iconValues.svg}
+              </li>
+            ) : (
+              ""
+            );
+          });
+        })}
+      </div> */}
     </div>
   );
 };
